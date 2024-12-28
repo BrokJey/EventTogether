f338bb4dab3d04f044bb601fc8d6d55f9adc5c9d
Danil <danilminka5@gmail.com>
2024-12-28 09:17:06 +0300

    final

--- templatess/header.php
+++ templatess/header.php
@@ -4,18 +4,21 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 ?>
 
+<?php
+$base_url = '/EventTogether'; // Корень вашего проекта
+?>
 <header>
     <nav>
-        <a href="../index.php">Главная</a>
+        <a href="<?= $base_url ?>/index.php">Главная</a>
         <?php if (isset($_SESSION['user_id'])): ?>
-            <a href="php/profile.php">Профиль</a>
-            <a href="php/chat.php">Чаты</a>
-            <a href="php/create_event.php">Создать событие</a>
-            <a href="php/my_events.php">Мои мероприятия</a>
-            <a href="php/logout.php">Выход</a>
+            <a href="<?= $base_url ?>/php/profile.php">Профиль</a>
+            <a href="<?= $base_url ?>/php/chat.php">Чаты</a>
+            <a href="<?= $base_url ?>/php/create_event.php">Создать событие</a>
+            <a href="<?= $base_url ?>/php/my_events.php">Мои мероприятия</a>
+            <a href="<?= $base_url ?>/php/logout.php">Выход</a>
         <?php else: ?>
-            <a href="php/login.php">Вход</a>
-            <a href="php/register.php">Регистрация</a>
+            <a href="<?= $base_url ?>/php/login.php">Вход</a>
+            <a href="<?= $base_url ?>/php/register.php">Регистрация</a>
         <?php endif; ?>
     </nav>
 </header>
